fc84ded919d40350995f651ca8795767912f5c88
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-09-12 22:15:48 +0300

    feat: finish dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    if (typeof members === 'string') {
+      return members.trim().charAt(0).toUpperCase();
+    } else {
+      return false;
+    }
+  } else {
+    const stringMembers = members.filter(item => typeof item === 'string');
+    if (stringMembers.length == 0) {
+      return false;
+    } else {
+      return stringMembers
+      .map(item => item.trim().charAt(0).toUpperCase())
+      .sort()
+      .join('');
+    }
+  }
 }
+
