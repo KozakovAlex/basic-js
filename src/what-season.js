d0cc7e6913e5b643b220a955f8518300f77065f2
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-09-18 23:11:43 +0300

    feat: what season task finish

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  const monthNumber = date.getMonth();
+  if (monthNumber == false) throw new Error('Invalid date!');
+  if (monthNumber == 11 || monthNumber <= 1) {
+    return 'winter'
+  } else if (monthNumber > 1 && monthNumber <= 4) {
+    return 'spring'
+  } else if (monthNumber > 4 && monthNumber <= 7) {
+    return 'summer'
+  } else if (monthNumber > 7 && monthNumber <= 10) {
+    return 'autumn (fall)'
+  } else {
+    throw new Error('Invalid date!');
+  }
+
 }
